b8b48948ed520af8dac29ac6ed94da9f61771731
Trevor Rosenkilde <tcrosenk@asu.edu>
2016-02-21 18:08:09 -0700

    Initial Version

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,66 @@
+/** Calculator class that will perform simple arithmetic operations on a total value.
+ * 
+ * @author Trevor Rosenkilde
+ * @version Feb 21, 2016
+ * 
+ */
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/** Constructor that initializes total to be equal to 0 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** getTotal will return the current value of total
+	 * 
+	 * @return the total value
+	 */
+	public int getTotal () {
+		return total;
+	}
+	
+	/** Takes in a single integer to add to the total
+	 * 
+	 * @param value to be added to total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/** Takes a single integer to subtract from the total
+	 * 
+	 * @param value to be subtracted from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/** Takes a single integer to multiply the total by
+	 * 
+	 * @param value to multiple the total by
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/** Takes a single integer to divide the total by
+	 * if it is zero, total is set to zero
+	 * 
+	 * @param value to divide the total by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** It will return a string that has the history of operations performed
+	 * 
+	 * @return string of history of operations
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,64 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	    System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	    System.setErr(null);
+	}
+	
+	@Test
+	public void testGetTotal() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSubtract() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetHistory() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCalculator() {
+		Calculator testCalc = new Calculator();
+		assertNotNull(testCalc);
+	}
+}
